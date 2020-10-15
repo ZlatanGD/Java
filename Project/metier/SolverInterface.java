b2e5de902c59d16d5819740600e799dfd8ba224f
Zlatan Gojak <47949331+ZlatanGD@users.noreply.github.com>
2020-10-15 22:10:20 +0200

    Delete SolverInterface.java

--- Project/metier/SolverInterface.java
+++
@@ -1,7 +0,0 @@
-public interface SolverInterface {
-	public void set(Configuration config);
-	public Configuration getConfiguration();
-	public boolean stop();
-	public int getTotalSteps();
-	public void step();
-}
